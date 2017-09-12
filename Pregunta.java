a28a11b39c05dcab4e60b965f19e8ca226cb3309
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2017-09-11 19:47:10 -0500

    Se añadieron los metodos de la clase pregunta.

--- Pregunta.java
+++ Pregunta.java
@@ -5,10 +5,45 @@
  */
 public class Pregunta
 {
+    private String texto;
+    private String respuesta;
     /**
      * Este es el constructor de la clase Pregunta
      */
     public Pregunta()
+    {
+        
+    }
+    
+    /**
+     * Metodo asigna texto del cuestionario.
+     */
+    public void asignarTexto()
+    {
+        
+    }
+    
+    /**
+     * Metodo asigna respuesta del usuario.
+     */
+    public void asignarRespuesta()
+    {
+        
+    }
+    
+    /**
+     * Metodo verifica la respuesta del usuario
+     * @return boolean expresion si o no.
+     */
+    public void checarRespuesta()
+    {
+        
+    }
+    
+    /**
+     * Metodo imprime los resultados.
+     */
+    public void Imprimete()
     {
         
     }
