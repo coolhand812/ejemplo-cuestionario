b6313d96805485b9b938d7f4264058ee084b3e98
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2017-09-11 20:08:51 -0500

    Se añadio la clase Cuestionario.

---
+++ Cuestionario.java
@@ -0,0 +1,15 @@
+/**
+ * Clase Cuestionario
+ * @autor Joe Diaz
+ * @version 1.0 (09/11/2017)
+ */
+public class Cuestionario
+{
+    /**
+     * Metodo para aplicar preguntas
+     */
+    public Pregunta aplicarPregunta(Pregunta unaPregunta)
+    {
+        return unaPregunta;
+    }
+}
